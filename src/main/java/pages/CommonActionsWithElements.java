aee765861e607d83a7b10ff147bad6372b94b7ed
Kostantin <kostiakava@gmail.com>
2023-05-02 21:15:39 +0300

    adding common actions with elements class

---
+++ src/main/java/pages/CommonActionsWithElements.java
@@ -0,0 +1,84 @@
+package pages;
+
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+
+public class CommonActionsWithElements {
+
+    protected WebDriver webDriver;
+    Logger logger = Logger.getLogger(getClass());
+
+    WebDriverWait webDriverWait10, webDriverWait15;
+
+    public CommonActionsWithElements(WebDriver webDriver) {
+        this.webDriver = webDriver;
+        PageFactory.initElements(webDriver, this);
+        webDriverWait10 = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+        webDriverWait15 = new WebDriverWait(webDriver, Duration.ofSeconds(15));
+
+    }
+
+    protected void enterTextInToElement(WebElement webElement, String text) {
+        try {
+            webDriverWait15.until(ExpectedConditions.visibilityOf(webElement));
+            webElement.clear();
+            webElement.sendKeys(text);
+            logger.info(text + " was putted into element" + getElementName(webElement));
+        } catch (Exception e) {
+            printErrorAndStopTest(e);
+        }
+    }
+
+    protected void clickOnElement(WebElement webElement) {
+        try {
+            webDriverWait15.until(ExpectedConditions.elementToBeClickable(webElement));
+            String name = getElementName(webElement);
+            webElement.click();
+            logger.info(name + " Element was clicked");
+        } catch (Exception e) {
+            printErrorAndStopTest(e);
+        }
+    }
+
+    protected void selectTextInDropDown(WebElement dropDown, String visibleText) {
+        try {
+            Select select = new Select(dropDown);
+            select.selectByVisibleText(visibleText);
+            logger.info(visibleText + " was selected in dropdown");
+        } catch (Exception e) {
+            printErrorAndStopTest(e);
+        }
+    }
+
+    public static boolean isElementPresented(WebElement webElement) {
+        try {
+            return webElement.isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private String getElementName(WebElement webElement) {
+        try {
+            return webElement.getAccessibleName();
+        } catch (Exception e) {
+            return "";
+        }
+    }
+
+    private void printErrorAndStopTest(Exception e) {
+        logger.error("Cannot work with element " + e);
+        Assert.fail("Cannot work with element" + e);
+    }
+
+
+}
