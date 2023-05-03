66ad4a44f694b02fe3758f9db67cb9418b269a10
Kostantin <kostiakava@gmail.com>
2023-05-03 18:57:50 +0300

    adding parent page

---
+++ src/main/java/base/ParentPage.java
@@ -0,0 +1,11 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import pages.CommonActionsWithElements;
+
+public class ParentPage extends CommonActionsWithElements {
+
+    public ParentPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+}
